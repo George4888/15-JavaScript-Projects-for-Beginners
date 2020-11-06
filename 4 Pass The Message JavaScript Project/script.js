8112a6a79a95aaa73dc48a82e7cd00bc1adf5ccc
George4888 <DanGeorge4888@gmail.com>
2020-11-06 16:04:20 +0200

    pass message

---
+++ 4 Pass The Message JavaScript Project/script.js
@@ -0,0 +1,11 @@
+const submitBtn = document.getElementById('submit-btn');
+let messageTxt = document.getElementById('message');
+
+
+submitBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  let inputText = document.getElementById('empty-field').value;
+  messageTxt.innerHTML = inputText;
+  document.getElementById('empty-field').value = ' ';
+
+})
