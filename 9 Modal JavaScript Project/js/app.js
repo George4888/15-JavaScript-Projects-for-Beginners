9c34d5d9a16e56a15d33886970aa76334ba00576
George4888 <DanGeorge4888@gmail.com>
2020-11-11 15:24:15 +0200

    counter

--- 9 Modal JavaScript Project/js/app.js
+++ 9 Modal JavaScript Project/js/app.js
@@ -1,8 +1,8 @@
 function modal() {
   let storeItems = document.getElementsByClassName('store-item');
+  let boxContainer = document.querySelector('.lightbox-container');
+  let boxItem = document.querySelector('.lightbox-item');
   let storeImages = document.getElementsByClassName('store-img');
-  let boxContainer = document.getElementsByClassName('lightbox-container');
-  let boxItem = document.getElementsByClassName('lightbox-item');
     
   let imageList = [];
   let counter = 0;
@@ -13,10 +13,29 @@ function modal() {
   console.log(imageList);
 
   for (let item of storeItems) {
-    item.addEventListener('clcik', (event) => {
-      item = event.target.src;
-    })
+    item.addEventListener('click', (event) => {
+      let image = event.target.src;
+      boxItem.style.backgroundImage = `url(${image})`;
+      boxContainer.classList.add('show');
+      counter = imageList.indexOf(image);
+    });
   }
+
+  const rightBtn = document.querySelector('.btnRight');
+  const leftBtn = document.querySelector('.btnLeft');
+
+  rightBtn.addEventListener('click', () => {
+    counter++;
+    if(counter >= imageList.length) {
+      counter = 0;
+    }
+    boxItem.style.backgroundImage = `url(${imageList[counter]})`;
+  })
+
+  leftBtn.addEventListener('click', () => {
+    counter--;
+    console.log(counter);
+  })
 }
 
 modal();
