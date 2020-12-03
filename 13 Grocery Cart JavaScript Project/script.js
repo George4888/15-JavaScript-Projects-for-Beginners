b9434d312004923870d3dc3350c976e27590de15
George4888 <dangeorge4888@gmail.com>
2020-12-03 10:33:13 +0200

    add button func

--- 13 Grocery Cart JavaScript Project/script.js
+++ 13 Grocery Cart JavaScript Project/script.js
@@ -1,5 +1,33 @@
 console.log('hello');
 
-const addButton = document.querySelector('.cart-btn');
-const buyButton = document.getElementById('clear');
+const shoppingCart = document.querySelector('.cart-items');
+const addButton = document.querySelectorAll('.cart-btn');
+const clearButton = document.getElementById('clear');
 const buyButton = document.getElementById('buy');
+
+addButton.forEach(button => {
+    button.addEventListener('click', createSelectedItem);
+});
+
+function createSelectedItem() {
+    const itemDiv = document.createElement('div');
+    itemDiv.classList.add('item');
+
+    const name = document.createElement('p');
+    name.innerHTML = 'item name';
+
+    const photo = document.createElement('img');
+    photo.innerHTML = `$`;
+
+    const desc = document.createElement('p');
+    desc.innerHTML = 'Short desc: Lorem ipsum dolor sit, amet consectetur adipisicing.';
+
+    const price = document.createElement('p');
+    price.innerHTML = `$`;
+
+    shoppingCart.appendChild(itemDiv);
+    itemDiv.appendChild(name);
+    itemDiv.appendChild(photo);
+    itemDiv.appendChild(desc);
+    itemDiv.appendChild(price);
+}
