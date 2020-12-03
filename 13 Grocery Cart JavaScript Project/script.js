6e50e66d8f2fa7f52a4a3ae827fcf53f950cef07
George4888 <dangeorge4888@gmail.com>
2020-12-03 10:08:01 +0200

    k

--- 13 Grocery Cart JavaScript Project/script.js
+++ 13 Grocery Cart JavaScript Project/script.js
@@ -1,4 +1,5 @@
 console.log('hello');
 
 const addButton = document.querySelector('.cart-btn');
-const buyButton = document.querySelector('.clear-buy');
+const buyButton = document.getElementById('clear');
+const buyButton = document.getElementById('buy');
