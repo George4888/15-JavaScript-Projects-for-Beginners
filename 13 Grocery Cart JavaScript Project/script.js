e662ac61b8192df7f77c363674fb71a17fae6b16
George4888 <dangeorge4888@gmail.com>
2020-12-03 11:25:27 +0200

    pass item to shopping cart

--- 13 Grocery Cart JavaScript Project/script.js
+++ 13 Grocery Cart JavaScript Project/script.js
@@ -9,21 +9,26 @@ addButton.forEach(button => {
     button.addEventListener('click', createSelectedItem);
 });
 
-function createSelectedItem() {
+function createSelectedItem(event) {
+    const selectedimage = event.target.parentElement;
+    const itemPrice = event.target.parentElement.childNodes[7].childNodes[1].innerText;
+    const img = selectedimage.childNodes[3].src;
+    console.log(itemPrice);
+
     const itemDiv = document.createElement('div');
-    itemDiv.classList.add('item');
+    itemDiv.classList.add('cart-item');
 
     const name = document.createElement('p');
     name.innerHTML = 'item name';
 
     const photo = document.createElement('img');
-    photo.innerHTML = `$`;
+    photo.setAttribute('src', `./images${img.slice(-9)}`);
 
     const desc = document.createElement('p');
     desc.innerHTML = 'Short desc: Lorem ipsum dolor sit, amet consectetur adipisicing.';
 
     const price = document.createElement('p');
-    price.innerHTML = `$`;
+    price.innerHTML = `Price: <span>${itemPrice}</span>$`;
 
     shoppingCart.appendChild(itemDiv);
     itemDiv.appendChild(name);
