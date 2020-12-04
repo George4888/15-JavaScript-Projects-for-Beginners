96e75302ea1d3bceb19fae3e8019ef66c1b68fb3
George4888 <dangeorge4888@gmail.com>
2020-12-04 11:58:52 +0200

    calculate total from shopping cart

--- 13 Grocery Cart JavaScript Project/script.js
+++ 13 Grocery Cart JavaScript Project/script.js
@@ -1,5 +1,3 @@
-console.log('hello');
-
 const shoppingCart = document.querySelector('.cart-items');
 const addButton = document.querySelectorAll('.cart-btn');
 const clearButton = document.getElementById('clear');
@@ -13,7 +11,6 @@ function createSelectedItem(event) {
     const selectedimage = event.target.parentElement;
     const itemPrice = event.target.parentElement.childNodes[7].childNodes[1].innerText;
     const img = selectedimage.childNodes[3].src;
-    console.log(itemPrice);
 
     const itemDiv = document.createElement('div');
     itemDiv.classList.add('cart-item');
@@ -28,11 +25,29 @@ function createSelectedItem(event) {
     desc.innerHTML = 'Short desc: Lorem ipsum dolor sit, amet consectetur adipisicing.';
 
     const price = document.createElement('p');
-    price.innerHTML = `Price: <span>${itemPrice}</span>$`;
+    price.innerHTML = `Price: <span class='cart-item-price'>${itemPrice}</span>$`;
 
     shoppingCart.appendChild(itemDiv);
     itemDiv.appendChild(name);
     itemDiv.appendChild(photo);
     itemDiv.appendChild(desc);
     itemDiv.appendChild(price);
+
+    calcTotal();
+}
+
+function calcTotal() {
+    const total = [];
+    const items = document.querySelectorAll('.cart-item-price');
+    
+    items.forEach(item => {
+        total.push(parseInt(item.textContent));
+    });
+    
+    const totalMoney = total.reduce((total, item) => {
+        total = total + item;
+        return total;
+    }, 0);
+
+    document.querySelector('#total').textContent = totalMoney;
 }
